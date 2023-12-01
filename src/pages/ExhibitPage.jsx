10824a66514d03e911ed7e621230dae1ff73ae93
unknown <chtiouimal@gmail.com>
2023-12-01 14:29:20 +0100

    canvas empty state and loading state

--- src/pages/ExhibitPage.jsx
+++ src/pages/ExhibitPage.jsx
@@ -4,10 +4,17 @@ import { Grid } from "@react-three/drei"
 import useAxios from "../hooks/useAxios"
 import { useEffect, useState } from "react"
 import Model from "../components/canvas/Model"
+import { Modal } from "antd"
+import IMG from "../assets/empty-state.svg"
+import { useNavigate } from "react-router-dom"
+import {LoadingOutlined} from "@ant-design/icons"
 
 const ExhibitPage = () => {
   const {data, loading, error, get} = useAxios()
   const [selectedList, setSelectedList] = useState([])
+  const [emptyState, setEmptyState] = useState(false);
+
+  const navigate = useNavigate()
   
   useEffect(() => {
     get(import.meta.env.VITE_SELECTED);
@@ -16,11 +23,18 @@ const ExhibitPage = () => {
   useEffect(() => {
     if(!loading) {
       setSelectedList(data)
+      if (data.length === 0) {
+        setEmptyState(true)
+      }
     }
   },[loading])
 
-  return (
-    // <AudioContextProvider>    
+  const handleNavigation = () => {
+    setEmptyState(false)
+    navigate("/settings")
+  }
+
+  return (   
     <>  
       <Canvas>
         <EnvPreset />
@@ -31,8 +45,20 @@ const ExhibitPage = () => {
       </Canvas>
       <BackDrop />
       <AudioPlayerGUI />
+      <Modal className="mc-empty-state" open={emptyState} centered footer={null} closeIcon={false}>
+        <div className="mc-empty-state-img">
+          <img src={IMG} alt="empty-state" />
+        </div>
+        <div className="mc-empty-state-desc">
+          <h3>No item selected</h3>
+          <span>Please go to settings and selected the items u want to showcase</span>
+          <button className="mc-btn mc-btn-primary" onClick={handleNavigation}>
+            Go to settings
+          </button>
+        </div>
+      </Modal>
+      {loading ? <div className="mc-page-loading"><LoadingOutlined /><h4>Loading...</h4></div> : null}
     </>
-    // </AudioContextProvider>
   )
 }
 

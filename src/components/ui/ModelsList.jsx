0d7aaa8f64e8f1211e2dafff17be38ccd884bada
unknown <chtiouimal@gmail.com>
2024-07-09 16:10:57 +0100

    change env variable

--- src/components/ui/ModelsList.jsx
+++ src/components/ui/ModelsList.jsx
@@ -88,6 +88,7 @@ const ModelsList = () => {
   return (
     <div className="mc-model-list">
       <header>
+        // search bar inactive at the moment
         {/* <Search
           className="mc-search-input"
           placeholder="Search by song title"

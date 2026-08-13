20794fbc39879db240018e664efb5b5a607c2527
chtiouimal <107561269+chtiouimal@users.noreply.github.com>
2026-08-13 16:27:52 +0100

    Update LayoutFooter.jsx

--- src/components/ui/LayoutFooter.jsx
+++ src/components/ui/LayoutFooter.jsx
@@ -6,7 +6,7 @@ const LayoutFooter = () => {
             <a className="ex-icon-link" href="https://www.instagram.com/chtioui.malek/" target="_blank">instagram</a>
         </div>
         <div className="mc-main-layout--bottom-center">
-            <a className="ex-icon-link" href="https://www.linkedin.com/in/malek-chtioui-282bb8161/" target="_blank">Linkedin</a>
+            <a className="ex-icon-link" href="https://www.linkedin.com/in/malek-chtioui-58bbb3278/" target="_blank">Linkedin</a>
         </div>
         <div className="mc-main-layout--bottom-right">
             <a className="ex-icon-link" href="https://wa.me/21622291232" target="_blank">whatsapp</a>
